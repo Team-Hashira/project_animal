dacf63f0203f5a18861731d610da553ec8f37fd7
Qmais <123608134+Qmais@users.noreply.github.com>
2024-11-20 18:33:48 +0900

    feat: uiSelectCanvas

---
+++ Assets/01.Scripts/Managers/UIScripts/UI_Popup.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class UI_Popup : UI_Base
+{
+    protected override bool Init()
+    {
+        if (base.Init() == false)
+            return false;
+
+        Managers.UI.SetCanvas(gameObject, false);
+        return true;
+    }
+}
