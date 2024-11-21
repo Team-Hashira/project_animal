f9a0b2a2b8e6b1c9d60ddb58f33ec2de41333128
Crogen0407 <mymcoding0407@gmail.com>
2024-11-22 03:01:39 +0900

    feat : Enemy Attack

--- Assets/01.Scripts/Unit/Components/AttackCompo.cs
+++ Assets/01.Scripts/Unit/Components/AttackCompo.cs
@@ -4,7 +4,12 @@ public class AttackCompo : MonoBehaviour, IAfterInitComponent
 {
 	[SerializeField] private DamageCaster2D _damageCaster;
 	[SerializeField] private int _damage;
+	[SerializeField] private float _attackDelay = 0.4f;
+	[SerializeField] private Transform _attackChargeVisualTrm;
+	private float _curAttackTime = 0f;
+
 	private StatCompo _statCompo;
+	private SurfaceMovementCompo _surfaceMovementCompo;
 
 	public void AfterInit()
 	{
@@ -13,6 +18,21 @@ public class AttackCompo : MonoBehaviour, IAfterInitComponent
 	public void Initialize(Entity entity)
 	{
 		_statCompo = entity.GetCompo<StatCompo>();
+	}
+
+	private void Update()
+	{
+		_attackChargeVisualTrm.localScale = Vector3.one * (_curAttackTime / _attackDelay);
+
+		if (_attackDelay < _curAttackTime)
+		{
+			OnAttack();
+			_curAttackTime = 0f;
+		}
+		else
+		{
+			_curAttackTime += Time.deltaTime;
+		}
 	}
 
 	public void OnAttack()
